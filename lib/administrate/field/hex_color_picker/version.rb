c6831bbbede5fe2d88aad58bf056efbf60cf5d2d
Jason Gorst <jason.gorst@me.com>
2024-02-10 12:48:56 -0600

    Bump administrate-field-hex_color_picker to 0.2.0.pre.3

--- lib/administrate/field/hex_color_picker/version.rb
+++ lib/administrate/field/hex_color_picker/version.rb
@@ -3,7 +3,7 @@ require "administrate/field/base"
 module Administrate
   module Field
     class HexColorPicker < Base
-      VERSION = "0.2.0.pre.2".freeze
+      VERSION = "0.2.0.pre.3".freeze
     end
   end
 end
