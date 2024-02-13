f0fc7dfca9b9a708e6f41897e0aec49a65d530df
Jason Gorst <jason.gorst@me.com>
2024-02-13 01:07:51 -0600

    use postcss-url to copy assets to app/assets/builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
+const path = require("node:path")
+
 module.exports = {
+  map: {
+    inline: false
+  },
   plugins: [
     require("postcss-import"),
+    require("postcss-url")({
+      url: "copy",
+      basePath: path.resolve("node_modules/@claviska/jquery-minicolors"),
+      assetsPath: ".",
+      useHash: true,
+      hashOptions: {
+        append: true
+      }
+    }),
     require("autoprefixer")
   ]
 }
