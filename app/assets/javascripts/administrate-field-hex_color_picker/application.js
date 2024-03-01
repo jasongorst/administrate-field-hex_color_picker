fd283679c60a02a0e1892310218fa4607d64f225
Jason Gorst <jason.gorst@me.com>
2024-03-01 00:21:33 -0600

    build with --external:jquery

--- app/assets/javascripts/administrate-field-hex_color_picker/application.js
+++ app/assets/javascripts/administrate-field-hex_color_picker/application.js
@@ -1,9 +1,8 @@
 import jQuery from "jquery"
 window.$ = jQuery
-window.jquery = jQuery
 
 import "@claviska/jquery-minicolors/jquery.minicolors"
 
-$(function() {
-  $('.minicolors').minicolors()
+window.$(function() {
+  window.$('.minicolors').minicolors()
 })
